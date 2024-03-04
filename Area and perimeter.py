a75abd69a0a12477e28e43d89439510d51d156cf
atmanapsar-48 <154489393+atmanapsar-48@users.noreply.github.com>
2024-03-04 08:15:02 +0530

    Create Area and perimeter.py

---
+++ Area and perimeter.py
@@ -0,0 +1,7 @@
+import math
+r=eval(input("Enter the value: "))
+Area=math.pi*(r*r)
+perimeter=2*math.pi*(r*r)
+print("The area of the circle: ",Area)
+print("The perimeter of the circle: ",perimeter)
+    
